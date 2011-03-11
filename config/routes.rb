18a093907ae74fc6f4137e642870ab38fcde980d
Oliver Arce <oliver@shownearby.com>
2011-03-11 19:04:00 +0800

    Done with the demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 DemoApp::Application.routes.draw do
+  
+  resources :microposts
+  
+  resources :users
+
+  get "home/index"
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +57,6 @@ DemoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
